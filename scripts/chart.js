a5b0ce0a45f99d951d941731f51c986b2124c7d1
ayc12345 <august.chen@outlook.com>
2020-06-20 17:39:15 -0700

    edited chart

--- scripts/chart.js
+++ scripts/chart.js
@@ -1,10 +1,9 @@
-
-
-
-
 window.onload = function () {
-	var time_spent = [["reddit.com",52],["youtube.com",103],["facebook.com",134]];
+	// var time_spent = 
+	// [["reddit.com",52],["youtube.com",103],["facebook.com",134]];
 	var testPoints = [];
+
+	// from: https://canvasjs.com/html5-javascript-bar-chart/
 	var chart = new CanvasJS.Chart("chartContainer", {
 		animationEnabled: true,
 		
@@ -27,23 +26,27 @@ window.onload = function () {
 			dataPoints: testPoints
 		}]
 	});
-	
 
 	chart.render();
 
-
 	var updateChart = function () {
-		
-
 			console.log("It worked");
 
 			// Reset data points
 			testPoints = [];
 
 			// Retrieve stored time spent and place new object into testPoints
-			chrome.storage.sync.get(['time_spent'], function(result){
+			/**chrome.storage.sync.get(['time_spent'], function(result){
 				for (var i = 0; i<result.length; i++){
 					testPoints.push({y:result[i][1], label:result[i][0]});
+				}
+				console.log(result);
+				console.log("changed");
+			})*/
+			chrome.storage.sync.get("time_spent", (response) => {
+				alert(response);
+				for (let i = 0; i < response.length; i++) {
+					testPoints.push({y:response[i][1], label:response[i][0]});
 				}
 				console.log(result);
 				console.log("changed");
@@ -78,6 +81,4 @@ window.onload = function () {
 
 	// Make chart reset every second
 	setInterval(function(){updateChart()},1000);
-
-
 }
